36e928d5e485a073357d407e2c5c177d7c17bcf2
cr0c0pk1 <alpics1337@gmail.com>
2021-10-17 13:04:32 +0300

    feat: get address info by hash from P-chain

--- src/modules/p-chain.ts
+++ src/modules/p-chain.ts
@@ -14,3 +14,34 @@ export async function getTransactionByIdFromPChain(txId: string) {
     
     return response.data;
 }
+
+export async function getAddressInfoFromPChain(address: string) {
+    let balanceResult;
+
+    await axios.post(process.env.P_CHAIN_BC_CLIENT_BLOCK_ENDPOINT, {
+        jsonrpc: '2.0',
+        id: 1,
+        method: 'platform.getBalance',
+        params: {
+                address: `${address}`
+        }
+    }, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*'
+        },
+    }).then(response => {
+        balanceResult = [0, response.data.result];
+    }).catch(error => {
+        if(!error.response) {
+            console.log("connection refused to avalanche client");
+            balanceResult = [1, JSON.parse('{"result":"connection refused to avalanche client"}')];
+        } else {
+            console.log(error.response.data);
+            balanceResult = [1, error.response.data];
+        }
+    });
+
+    return balanceResult;
+}
+
